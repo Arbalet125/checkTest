85e51b70f506defe1741c26072a702c703001fd1
Arbalet <MametovV@gmail.com>
2022-08-06 15:57:15 +0300

    added inputs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine($"Введите число для сравнения и нажмите Enter");
+int compareNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите число, отображающее длину изначального массива и нажмите Enter");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+string [] array = new string [arrayLength];
+
+int newArraySize = 0;
