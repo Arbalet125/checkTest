c385d6a6b59f57e7e8ac3aae6af932079fa19f43
Arbalet <MametovV@gmail.com>
2022-08-06 15:58:20 +0300

    added first cycle

--- Program.cs
+++ Program.cs
@@ -7,3 +7,16 @@ int arrayLength = Convert.ToInt32(Console.ReadLine());
 string [] array = new string [arrayLength];
 
 int newArraySize = 0;
+
+for (int i = 0; i < arrayLength; i++)
+{
+    Console.WriteLine($"Введите {i}-ый элемент массива и нажмите Enter (если вы ничего не введете, элемент массива будет засчитан, как 0)");
+    array[i] = Console.ReadLine();
+    if (array[i] is null) array[i] = "0";
+    string firstArrayElement = array[i];
+    int firstArrayElementLength = firstArrayElement.Length;
+    if (firstArrayElementLength <= compareNumber)
+    {
+        newArraySize ++;
+    }
+}
